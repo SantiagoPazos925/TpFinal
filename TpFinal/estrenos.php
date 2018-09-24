0102dbab9daef774974aa72a1868342f5588d3b8
SantiagoPazos925 <santiagopazos925@gmail.com>
2018-09-24 11:38:41 -0300

    este es el nuevo tp

---
+++ TpFinal/estrenos.php
@@ -0,0 +1,65 @@
+<div class="container estrenos">
+  <div class="row ">
+    <div class="col-12 tituloEstreno">
+      <h2>Ultimos <span style="color:rgb(203, 51, 42)">Estrenos</span</h2>
+    </div>
+  </div>
+    <div class="row  portadasEstrenos">
+      <div class="col-6 uno">
+        <div class="contenedorImg">
+            <img class=portrait src="images/CoDBO3.jpg" alt="">
+            <div class="row col-12 precioCarro">
+              <div class="col-6 precio">
+                <p>$2000</p>
+              </div>
+              <div class="col-6 carro">
+                <i class="ion-android-cart"></i>
+              </div>
+            </div>
+
+
+        </div>
+        <div class="contenedorImg">
+            <img class=portrait src="images/fallout76.jpg" alt="">
+            <div class="row precioCarro">
+                <div class="col-6 precio">
+                  <h4>$2000</h4>
+                </div>
+                <div class="col-6 carro">
+                  <i class="ion-android-cart"></i>
+                </div>
+            </div>
+        </div>
+      </div>
+      <div class="col-6 dos">
+        <div class="contenedorImg">
+            <img class=portrait src="images/MetroExodus.jpg" alt="">
+            <div class="row precioCarro">
+                <div class="col-6 precio">
+                  <h4>$2000</h4>
+                </div>
+                <div class="col-6 carro">
+                  <i class="ion-android-cart"></i>
+                </div>
+            </div>
+        </div>
+
+        <div class="contenedorImg">
+            <img class=portrait src="images/pes19.jpg" alt="">
+            <div class="row precioCarro">
+                <div class="col-6 precio">
+                  <h4>$2000</h4>
+                </div>
+                <div class="col-6 carro">
+                  <i class="ion-android-cart"></i>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="row ">
+      <div class="col-12 separador">
+
+      </div>
+    </div>
+</div>
---
+++ TpFinal/header.php
@@ -0,0 +1,25 @@
+<div class="container-fluid header ">
+
+
+<div class="row">
+  <div class="col-6 logo">
+    <h2><span style='color:rgb(203, 51, 42)'>Digital</span>Games</h2>
+  </div>
+  <div class="col-6 iconos">
+
+    <i class="ion-android-cart"></i>
+    <i class="ion-navicon-round"></i>
+  </div>
+  </div>
+  <div class="row search offset-1">
+    <div  class="col-9 searching ">
+
+      <input type="search" name="" value="" placeholder="Busca tu juego aqui">
+    </div>
+    <div class="col-1 iconSearch">
+      <i class="ion-search"></i>
+    </div>
+
+    </div>
+  </div>
+</div>
